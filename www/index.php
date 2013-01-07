b35f3c29a1e2c55a6c9f2c4c4c4257a522956e14
Mike Capstack <walrus@mikecapstack.co.uk>
2013-01-07 12:23:32 +0000

    initial commit of todo class

--- www/index.php
+++ www/index.php
@@ -1,6 +1,7 @@
 <html>
   <?php
     include('functions.php');
+    include('todo.php');
   ?>
   <head>
   </head>
@@ -8,12 +9,16 @@
 
   <?php
 
-$todos = todo_all();
+/*$todos = todo_all();
  
       $items = array();
       foreach($todos as $todo) {
         $items[] = $todo['ID'] . ' ' . $todo['Title'] . ' ' . $todo['Description'] . ' ' . $todo['Priority'];
       }
       print theme_list($items);
+*/
+
+$myTodo= new Todo(5);
+print $myTodo->renderHtml();
 
     
